9e7586e4f213ae6c2fe2ef4cd601d6a4383afce2
Dan Miller <dan@danielsmiller.com>
2013-03-15 15:31:34 -0400

    learn2program sequentially

--- lib/plugins/plus.rb
+++ lib/plugins/plus.rb
@@ -7,12 +7,13 @@ class Plus
   match(/\+\+ (.+)/)
   def execute(m, target)
     r = RedisStorage.new(self)
-    total = r[target]
     if (target == m.user.nick)
       r.decr(target)
+      total = r[target]
       m.reply("No cheating #{m.user.nick}! -1 Plus!")
     else
       r.incr(target)
+      total = r[target]
       m.reply(target.to_s + " got a plus! " + target.to_s + " now has " + total.to_s + " plusses!")
     end
   end
