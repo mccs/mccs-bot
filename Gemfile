dfe86b877ba034e8de9ac2ada6c57b25054a4101
Dan Miller <dan@danielsmiller.com>
2013-03-16 00:05:12 -0400

    More pluses

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'cinch'
 gem 'redis'
+gem 'leaderboard'
--- lib/plugins/plus.rb
+++ lib/plugins/plus.rb
@@ -1,20 +1,40 @@
 require 'cinch'
+require 'leaderboard'
 require './lib/redis_storage'
 
 class Plus
   include Cinch::Plugin
 
-  match(/\+\+ (.+)/)
-  def execute(m, target)
-    r = RedisStorage.new(self)
+
+  match(/\+\+ (.+)/, method: :plus)
+  def plus(m, target)
+    @plus_lb = Leaderboard.new('pluses')
     if (target == m.user.nick)
-      r.decr(target)
-      total = r[target]
+      @plus_lb.change_score_for(target, -1)
+      total = @plus_lb.score_for(target)
       m.reply("No cheating #{m.user.nick}! -1 Plus!")
     else
-      r.incr(target)
-      total = r[target]
-      m.reply(target.to_s + " got a plus! " + target.to_s + " now has " + total.to_s + " plusses!")
+      @plus_lb.change_score_for(target, 1)
+      total = @plus_lb.score_for(target)
+      m.reply(target.to_s + " got a plus! " + target.to_s + " now has " + total.to_s + " pluses!")
     end
+  end
+
+  match(/pluses (.+)/, method: :get_plus)
+  def get_plus(m, target)
+    @plus_lb = Leaderboard.new('pluses')
+    total = @plus_lb.score_for(target)
+    m.reply("You have " + total.to_s + " plusses")
+  end
+
+  match(/plus_leaders/, method: :get_leaders)
+  def get_leaders(m)
+    @plus_lb = Leaderboard.new('pluses')
+    leaders = @plus_lb.leaders(1)
+    leader_string = ''
+    leaders.each do |leader|
+      leader_string = leader_string + leader[:rank].to_s + ". " + leader[:member] + ": " + leader[:score].to_s + ", "
+    end
+    m.reply(leader_string)
   end
 end
