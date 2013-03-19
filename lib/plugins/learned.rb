2bd838d0cf05201dda5401abdb8794b69101089d
Colby Rabideau <crabideau5691@gmail.com>
2013-03-18 21:47:24 -0400

    doin redis stuff

--- lib/plugins/learned.rb
+++ lib/plugins/learned.rb
@@ -4,6 +4,15 @@ require './lib/redis_storage'
 class Learn
   include Cinch::Plugin
 
+  def initialize
+    @store = RedisStorage.new(@self)
+  end
+
+  match(/learn url (.+) "(.+)"/, method: :learn)
+  def learn_img(m, cmd, response)
+    m.reply("Okay, learned " + cmd + " => " + response)
+  end
+
   match(/learn (.+) "(.+)"/, method: :learn)
   def learn(m, cmd, response)
     m.reply("Okay, learned " + cmd + " => " + response)
