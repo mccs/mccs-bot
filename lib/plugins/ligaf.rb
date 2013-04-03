4a94e0cd4e5ef544108bf03bfe50a3d5dd95f88e
Dan Miller <dan@danielsmiller.com>
2013-04-02 22:43:14 -0400

    content

--- lib/plugins/ligaf.rb
+++ lib/plugins/ligaf.rb
@@ -5,6 +5,7 @@ class Ligaf
 
   match('ligaf')
   def execute(m)
-    m.reply "http://www.troll.me/images/rainbow-spongebob/like-i-give-a-fuck.jpg"
+    ligafs = ["http://images3.wikia.nocookie.net/__cb20120221212715/smuff/images/6/6c/LIGAF.gif", "http://www.troll.me/images/rainbow-spongebob/like-i-give-a-fuck.jpg"]
+    m.reply ligafs.sample
   end
 end
