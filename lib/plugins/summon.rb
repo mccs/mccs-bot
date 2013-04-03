db8ecb24512a1bf55a00b973dcf9c9a340599136
Dan Miller <dan@danielsmiller.com>
2013-04-03 01:26:07 -0400

    Resummon

--- lib/plugins/summon.rb
+++ lib/plugins/summon.rb
@@ -4,9 +4,16 @@ require 'google-search'
 class Summon
   include Cinch::Plugin
 
-  match(/summon (.+)/)
-  def execute(m, query)
+  match(/summon (.+)/, method: :single_summon)
+  def single_summon(m, query)
     image_results = Google::Search::Image.new(:query => query)
     m.reply(image_results.first.uri.to_s)
+  end
+
+  match(/resummon (.+)/, method: :resummon)
+  def resummon(m, query)
+    image_results = Google::Search::Image.new(:query => query)
+    image_results.shift
+    m.reply(image_results.sample.uri.to_s)
   end
 end
