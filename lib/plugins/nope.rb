d29bcfd40ce6e3fe70310decb1d09a6a42bda0d9
Garrett Oreilly <oreillygarrett@gmail.com>
2013-05-19 16:42:16 -0400

    added gif

--- lib/plugins/nope.rb
+++ lib/plugins/nope.rb
@@ -5,7 +5,7 @@ class Nope
 
   match('nope')
   def execute(m)
-    nopes = ["http://i.imgur.com/ni4kyJu.gif"]
+    nopes = ["http://i.imgur.com/ni4kyJu.gif", "http://img6.imagebanana.com/img/cj8juagu/tumblr_lope3fakj51qip78p.gif"]
     m.reply nopes.sample
   end
 end
