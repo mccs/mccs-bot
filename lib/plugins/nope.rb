d7438e6aeae6429c815abf39201f12c3915a437e
Garrett Oreilly <oreillygarrett@gmail.com>
2013-05-19 21:54:33 -0400

    reverted command for simplicity

--- lib/plugins/nope.rb
+++ lib/plugins/nope.rb
@@ -3,7 +3,7 @@ require 'cinch'
 class Nope
   include Cinch::Plugin
 
-  match('nope.avi')
+  match('nope')
   def execute(m)
     nopes = ["http://img6.imagebanana.com/img/cj8juagu/tumblr_lope3fakj51qip78p.gif"]
     m.reply nopes.sample
