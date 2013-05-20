7d42069caac65feec9b27751992e96701b2412e0
Garrett Oreilly <oreillygarrett@gmail.com>
2013-05-19 20:43:43 -0400

    updated command to nope.avi

--- lib/plugins/nope.rb
+++ lib/plugins/nope.rb
@@ -3,7 +3,7 @@ require 'cinch'
 class Nope
   include Cinch::Plugin
 
-  match('nope')
+  match('nope.avi')
   def execute(m)
     nopes = ["http://i.imgur.com/ni4kyJu.gif", "http://img6.imagebanana.com/img/cj8juagu/tumblr_lope3fakj51qip78p.gif"]
     m.reply nopes.sample
