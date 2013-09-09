0c80706c5631da31aeb7e16ebcf8b02116944e74
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-09-09 17:10:51 -0400

    moar gifs.

--- lib/plugins/ligaf.rb
+++ lib/plugins/ligaf.rb
@@ -5,7 +5,13 @@ class Ligaf
 
   match('ligaf')
   def execute(m)
-    ligafs = ["http://images3.wikia.nocookie.net/__cb20120221212715/smuff/images/6/6c/LIGAF.gif", "http://www.troll.me/images/rainbow-spongebob/like-i-give-a-fuck.jpg"]
+    ligafs = [
+      "http://images3.wikia.nocookie.net/__cb20120221212715/smuff/images/6/6c/LIGAF.gif",
+      "http://www.troll.me/images/rainbow-spongebob/like-i-give-a-fuck.jpg",
+      "http://bukk.it/allthefucks.gif",
+      "http://bukk.it/yukongiveafuck.png"
+    ]
+
     m.reply ligafs.sample
   end
 end
--- lib/plugins/nope.rb
+++ lib/plugins/nope.rb
@@ -5,7 +5,14 @@ class Nope
 
   match('nope')
   def execute(m)
-    nopes = ["http://img6.imagebanana.com/img/cj8juagu/tumblr_lope3fakj51qip78p.gif"]
+    nopes = [
+      "http://img6.imagebanana.com/img/cj8juagu/tumblr_lope3fakj51qip78p.gif",
+      "http://bukk.it/tracynope.gif",
+      "http://bukk.it/nuhnope.gif",
+      "http://i.imgur.com/R2v3c6H.gif",
+      "http://i.imgur.com/RMsnqCF.gif",
+      "http://i.imgur.com/0U2cnck.gif"
+    ]
     m.reply nopes.sample
   end
 end
