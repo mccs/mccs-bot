a07b5d650983634b89630323fab0dc385fc5d0a5
Garrett Oreilly <oreillygarrett@gmail.com>
2014-03-14 16:33:21 -0400

    Added wat plugin

---
+++ lib/plugins/wat.rb
@@ -0,0 +1,14 @@
+require 'cinch'
+require 'google-search'
+
+class Wat
+    include Cinch::Plugin
+
+    match('wat')
+    def execute(m)
+        image_results = Google::Search::Image.new(:query => 'wat')
+        image_results = image_results.to_a
+        image_results.shift
+        m.reply(image_results.sample.uri.to_s)
+    end
+end
